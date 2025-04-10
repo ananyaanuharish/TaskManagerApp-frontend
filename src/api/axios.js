3780782b8c55b4b9511472d23f70d12dec572f0f
Ananya A H <ananyaharish00@gmail.com>
2025-04-10 21:11:43 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskmanagerapp-backend-1.onrender.com",
+  baseURL: "https://taskmanagerapp-backend-1.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
